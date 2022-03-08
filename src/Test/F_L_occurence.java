e2dbbab1fcb39a657c6dcb302bcae2add7dfd18c
Mohana Balaji <97578960+mohanabalaji@users.noreply.github.com>
2022-03-08 04:07:27 -0800

    new

---
+++ src/Test/F_L_occurence.java
@@ -0,0 +1,37 @@
+package Test;
+
+public class F_L_occurence 
+{
+	public static void
+	flchar(String str)
+	{
+
+		int n = str.length();
+  
+		for(int i=0; i<=str.length();i++)
+		{
+			if()
+			{
+				
+				char f = 
+			}
+			
+		}
+		System.out.println("First: " + f);
+		System.out.println("Last: " + l);
+	}
+
+	public static void main(String[] args) 
+	{
+		String str = "Hello, World";
+		flchar(str);
+	}
+}
+
+
+
+	
+
+
+		
+
---
+++ src/vowelsandconsonants/Remove_duplicate.java
@@ -0,0 +1,12 @@
+package vowelsandconsonants;
+
+public class Remove_duplicate {
+
+	public static void main(String[] args) 
+	{
+		 
+	   }
+
+	}
+
+
